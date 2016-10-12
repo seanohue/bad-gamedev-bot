69cf08cec4ea1bfe64e0c89213cadfe7b43fa24e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-12 17:09:34 -0400

    Add vendor and gem stuff to gitignore. Try an initial test tweet

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,6 +1,10 @@
+require 'twitter'
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
+
+client.update("this was sent using a twitter ruby gem")
