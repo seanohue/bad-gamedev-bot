798d13bc193dc1602c4eafcd53b9d4df1f33d947
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-13 13:28:34 -0400

    Cruddy basic random strings being mashed together

---
+++ lib/bot/idea.rb
@@ -0,0 +1,58 @@
+
+starters = [
+  'i have good idea: ',
+  'dream last night, could be game: ',
+  'here is idea i had for game, it is ',
+  'here is game idea, ',
+  'best game idea today ',
+  'this morning, great idea ',
+  'shower idea for game ',
+  'game idea: ',
+  'another game idea: ',
+  'here is idea ',
+  'my new project is ',
+  'best project ',
+  'anyone want game '
+]
+
+characters = [
+  'moth',
+  'potato',
+  'communists',
+  'rich people',
+  'canaries',
+  'pancakes',
+  'gang members',
+  'superhero',
+  'boat'
+]
+
+settings = [
+  'a boat',
+  'big city town',
+  'ocean island',
+  'an airplane',
+  'future world',
+  'dinosaur land',
+  'dragon lair',
+  'wild west',
+  'a castle'
+]
+
+goals = [
+  'be biggest ' + characters.sample,
+  'beat up all the ' + characters.sample,
+  'become ' + (characters + settings).sample,
+  'find the ' + characters.sample,
+  'get to ' + settings.sample
+]
+
+contexts = [
+  'you play as ' + characters.sample,
+  'it is set in ' + settings.sample,
+  'enemies are ' + characters.sample,
+  'the goal is ' + goals.sample
+]
+
+blah = starters.sample + 'where ' + contexts.sample + ' and ' + contexts.sample
+puts(blah)
