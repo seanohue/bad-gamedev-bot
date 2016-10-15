f52616c0447234166a6a87cadd018af9b428976c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-15 14:21:36 -0400

    Put twitter client in class. Now it is a classy bot.

---
+++ lib/twitter-client.rb
@@ -0,0 +1,19 @@
+require 'twitter'
+
+class TwitterClient
+
+  def initialize()
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
+  end
+
+  def tweet(msg)
+    @client.update(msg)
+    puts("Tweeted: '#{msg}'")
+  end
+
+end
