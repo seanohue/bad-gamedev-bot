173bf037efb68609b8253f27ed4f970c4bd5bc95
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-16 16:54:14 -0400

    Add method for loading initial txt of awful ideas. Load it and make the path work and such.

--- lib/markov.rb
+++ lib/markov.rb
@@ -7,12 +7,20 @@ class Markov
   end
 
   def add_str_to_dict(str)
-    @dict.parse_string(str)
+    @dict.parse_string str
+  end
+
+  def add_file_to_dict(path)
+    @dict.parse_file path
   end
 
   def get_sentence_from_dict(n)
     n = n || 1
     return @dict.generate_n_sentences n
+  end
+
+  def add_bad_ideas
+    add_file_to_dict './data/bad-game-ideas.txt'
   end
 
 end
