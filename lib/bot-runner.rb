a18d73deffb40e26dc21b48f085f27a19bbf9f99
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-10-29 13:40:19 -0400

    Extract get tweet content method thing. Add a method to get twitter sources and stuff.

--- lib/bot-runner.rb
+++ lib/bot-runner.rb
@@ -9,10 +9,30 @@ class BotRunner
   end
 
   def get_timeline()
-    return @client.get_own_timeline.map do |tweet|
+    get_tweet_content(@client.get_own_timeline)
+  end
+
+  def get_tweet_content(tweets)
+    return tweets.map do |tweet|
       "#{tweet.text}"
     end
   end
 
-
+  def get_twitter_sources()
+    sources = [
+      terriblegameideas,
+      greatgameideas,
+      awfulgameideas,
+      gameideas,
+      mygameideas,
+      gameidea,
+      horrorgameidea,
+      rpgidea,
+      videogameidea
+    ].map do |hashtag|
+        @client.get_by_hashtag(hashtag)
+      end
+    return @client.get_ten_most_favorited(sources).flatten
+  end
+  
 end
